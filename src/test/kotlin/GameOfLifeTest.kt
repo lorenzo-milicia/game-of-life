9ab6138da4b835fee2cbb27adecebdd0a48126c3
Lorenzo, Milicia <lorenzo.milicia@cgm.com>
2021-09-23 17:17:11 +0200

    RED: a living cell surrounded by dead cells should die

---
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class GameOfLifeTest {
+
+	@Test
+	internal fun `a living cell surrounded by dead cells should die`() {
+		val input: List<Int> = listOf(
+			0,0,0,
+			0,1,0,
+			0,0,0
+		)
+
+		val output = evolve(input)
+
+		assertEquals(listOf(
+			0,0,0,
+			0,0,0,
+			0,0,0
+		), output)
+	}
+}
+
+fun evolve(initialState: List<Int>): List<Int> {
+	return listOf()
+}
