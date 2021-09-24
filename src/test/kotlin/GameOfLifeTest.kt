d0911c1bb3fa281525340f08a5ad196356108a97
Lorenzo, Milicia <lorenzo.milicia@cgm.com>
2021-09-24 09:47:56 +0200

    TESTS:  GREEN: a living cell surrounded by more than three living cells should die  GREEN: `a dead cell surrounded by three living cells should come back to life`

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -6,65 +6,104 @@ class GameOfLifeTest {
 	@Test
 	internal fun `a living cell surrounded by dead cells should die`() {
 		val input: List<Int> = listOf(
-			0,0,0,
-			0,1,0,
-			0,0,0
+			0, 0, 0,
+			0, 1, 0,
+			0, 0, 0
 		)
 
 		val output = evolve(input)
 
-		assertEquals(listOf(
-			0,0,0,
-			0,0,0,
-			0,0,0
-		), output)
+		assertEquals(
+			listOf(
+				0, 0, 0,
+				0, 0, 0,
+				0, 0, 0
+			), output)
 	}
 
 	@Test
 	internal fun `a living cell surrounded by two living cells should stay alive`() {
 		val input: List<Int> = listOf(
-			1,1,0,
-			0,1,0,
-			0,0,0
+			1, 1, 0,
+			0, 1, 0,
+			0, 0, 0
 		)
 
 		val output = evolve(input)
 
-		assertEquals(listOf(
-			1,1,0,
-			0,1,0,
-			0,0,0
-		), output)
+		assertEquals(
+			listOf(
+				1, 1, 0,
+				0, 1, 0,
+				0, 0, 0
+			), output)
+	}
+
+	@Test
+	internal fun `a living cell surrounded by more than three living cells should die`() {
+		val input: List<Int> = listOf(
+			1, 1, 1,
+			0, 1, 1,
+			0, 0, 0
+		)
+
+		val output = evolve(input)
+
+		assertEquals(
+			listOf(
+				1, 1, 1,
+				0, 0, 1,
+				0, 0, 0
+			), output)
+	}
+
+	@Test
+	internal fun `a dead cell surrounded by three living cells should come back to life`() {
+		val input: List<Int> = listOf(
+			1, 1, 1,
+			0, 0, 0,
+			0, 0, 0
+		)
+
+		val output = evolve(input)
+
+		assertEquals(
+			listOf(
+				1, 1, 1,
+				0, 1, 0,
+				0, 0, 0
+			), output)
 	}
 }
 
 fun evolve(initialState: List<Int>): List<Int> {
-	val cells: MutableList<Cell> = initialState.map { if (it == 0) Cell(CellState.DEAD) else Cell(CellState.ALIVE) } as MutableList<Cell>
+	val cells: MutableList<Cell> =
+		initialState.map { if (it == 0) Cell(CellState.DEAD) else Cell(CellState.ALIVE) } as MutableList<Cell>
 
 	var cell: Cell = cells.removeAt(4)
 	val neighbouringLivingCells = cells.count { it.isAlive }
 	cell = when {
-		neighbouringLivingCells > 3 -> Cell(CellState.ALIVE)
-		neighbouringLivingCells < 2 -> Cell()
+		neighbouringLivingCells > 3 -> Cell(CellState.DEAD)
+		neighbouringLivingCells < 2 -> Cell(CellState.DEAD)
 		else                        -> Cell(CellState.ALIVE)
 	}
 
 
 	cells.add(4, cell)
-	return cells.map { if(it.isAlive) 1 else 0 }
+	return cells.map { if (it.isAlive) 1 else 0 }
 }
 
 class Cell(
 	var state: CellState = CellState.DEAD
 ) {
+
 	val isAlive: Boolean
-	get() = state == CellState.ALIVE
+		get() = state == CellState.ALIVE
 
 	val isDead: Boolean
 		get() = state == CellState.DEAD
 
 }
-
 
 
 enum class CellState {
