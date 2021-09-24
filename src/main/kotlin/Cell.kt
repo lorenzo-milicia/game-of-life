497fdd351940120e7a46a1a028f1fa35b7be90dd
Lorenzo, Milicia <lorenzo.milicia@cgm.com>
2021-09-24 17:27:17 +0200

    Files that weren't added to the commit.

---
+++ src/main/kotlin/Cell.kt
@@ -0,0 +1,41 @@
+import CellState
+
+data class Cell(
+	private var state: CellState = CellState.DEAD
+) {
+
+	val isAlive: Boolean
+		get() = state == CellState.ALIVE
+
+	val isDead: Boolean
+		get() = state == CellState.DEAD
+
+	fun kill() {
+		state = CellState.DEAD
+	}
+
+	fun resuscitate() {
+		state = CellState.ALIVE
+	}
+
+	fun evolve(neighbouringCells: List<Cell>) {
+		val neighbouringLivingCells = neighbouringCells.count { it.isAlive }
+		val MAX_LIVING_NEIGHBOURING_CELLS = 3
+		val MIN_LIVING_NEIGHBOURING_CELLS = 2
+		val MAGIC_NUMBER_FOR_RESUSCITATING = 3
+
+		if (isAlive) {
+			when {
+				neighbouringLivingCells > MAX_LIVING_NEIGHBOURING_CELLS -> kill()
+				neighbouringLivingCells < MIN_LIVING_NEIGHBOURING_CELLS -> kill()
+				else                                                    -> Unit
+			}
+		}
+		else {
+			when (neighbouringLivingCells) {
+				MAGIC_NUMBER_FOR_RESUSCITATING -> resuscitate()
+				else                           -> Unit
+			}
+		}
+	}
+}
---
+++ src/main/kotlin/Evolver.kt
@@ -0,0 +1,53 @@
+object Evolver {
+	fun evolve(initialState: List<Int>): List<Int> {
+
+		val cells: MutableList<Cell> =
+			initialState.map { if (it == 1) Cell(CellState.ALIVE) else Cell(CellState.DEAD) }.toMutableList()
+
+		val cellsCopy = cells.toMutableList()
+
+		for ((index, cell) in cellsCopy.withIndex()) {
+			val column = index % 5
+			val row = index / 5
+			if (column == 0 || column == 4 || row == 0 || row == 4) Unit
+			else cell.evolve(listOf(
+				cells[5 * (row-1) + column - 1],
+				cells[5 * (row-1) + column    ],
+				cells[5 * (row-1) + column + 1],
+				cells[5 * (row  ) + column - 1],
+				cells[5 * (row  ) + column + 1],
+				cells[5 * (row+1) + column - 1],
+				cells[5 * (row+1) + column    ],
+				cells[5 * (row+1) + column + 1],
+			))
+		}
+
+		return cellsCopy.map { if (it.isAlive) 1 else 0 }
+	}
+
+	fun evolve(initialState: Array<Int>, dimension: Int): Array<Int> {
+
+		val cells: MutableList<Cell> =
+			initialState.map { if (it == 1) Cell(CellState.ALIVE) else Cell(CellState.DEAD) }.toMutableList()
+
+		val cellsCopy = cells.map { it.copy() }
+
+		for ((index, cell) in cellsCopy.withIndex()) {
+			val column = index % dimension
+			val row = index / dimension
+			if (column == 0 || column == dimension - 1 || row == 0 || row == dimension - 1) Unit
+			else cell.evolve(listOf(
+				cells[dimension * (row-1) + column - 1],
+				cells[dimension * (row-1) + column    ],
+				cells[dimension * (row-1) + column + 1],
+				cells[dimension * (row  ) + column - 1],
+				cells[dimension * (row  ) + column + 1],
+				cells[dimension * (row+1) + column - 1],
+				cells[dimension * (row+1) + column    ],
+				cells[dimension * (row+1) + column + 1],
+			))
+		}
+
+		return cellsCopy.map { if (it.isAlive) 1 else 0 }.toTypedArray()
+	}
+}
